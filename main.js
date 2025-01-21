ca4fd3fe0a7ed3704ade59ed9cd96535b1688435
fernandaatim <143547756+fernandaatim@users.noreply.github.com>
2025-01-21 11:47:54 -0300

    :tada:Initial Commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const form = document.querySelector('.form')
+const resultado = document.querySelector('#resultado')
+
+function processarDados(event){
+    event.preventDefault();
+
+    let altura = parseFloat(form.querySelector('#altura').value);  
+    let peso = parseFloat(form.querySelector('#peso').value);
+
+    calcularIMC(altura,peso)
+}
+
+function calcularIMC(altura,peso){
+    if (isNaN(altura) || isNaN(peso) || altura <= 0 || peso <= 0) {
+        alert("Por favor, insira valores válidos para altura e peso.");
+        return;
+    }
+
+    imc = peso / (altura*altura)
+    
+    resultadoIMC(imc)
+}
+
+function resultadoIMC(imc){
+    resultadoFinal = ''
+
+    if (imc < 18.5) {
+        resultadoFinal = `IMC: ${imc.toFixed(2)} - Abaixo do peso`;
+    } else if (imc >= 18.5 && imc < 24.9) {
+        resultadoFinal = `IMC: ${imc.toFixed(2)} - Peso normal`;
+    } else if (imc >= 25 && imc < 29.9) {
+        resultadoFinal = `IMC: ${imc.toFixed(2)} - Sobrepeso`;
+    } else if (imc >= 30 && imc < 34.9) {
+        resultadoFinal = `IMC: ${imc.toFixed(2)} - Obesidade grau 1`;
+    } else if (imc >= 35 && imc < 39.9) {
+        resultadoFinal = `IMC: ${imc.toFixed(2)} - Obesidade grau 2`;
+    } else {
+        resultadoFinal = `IMC: ${imc.toFixed(2)} - Obesidade grau 3`;
+    }
+
+    resultado.innerHTML += `<p>${resultadoFinal}</p>`
+}
+
+form.addEventListener('submit',processarDados)
